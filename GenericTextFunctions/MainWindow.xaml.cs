d14befacd47db9af021abd50d083ba6439e72b42
francoishill <francoishill@7a914abf-0711-2844-a2db-87731c227079>
2012-03-06 10:36:27 +0000

    Added integration for Application Manager.
    git-svn-id: svn://localhost/generictextfunctions/trunk@22 7a914abf-0711-2844-a2db-87731c227079

--- GenericTextFunctions/MainWindow.xaml.cs
+++ GenericTextFunctions/MainWindow.xaml.cs
@@ -17,6 +17,8 @@ using ITextOperation = GenericTextFunctions.TextOperations.ITextOperation;
 using System.Reflection;
 using System.Linq;
 using System.IO;
+using System.Windows.Interop;
+using SharedClasses;
 
 namespace GenericTextFunctions
 {
@@ -40,6 +42,8 @@ namespace GenericTextFunctions
 
 		private void Window_Loaded(object sender, RoutedEventArgs e)
 		{
+			WindowMessagesInterop.InitializeClientMessages();
+
 			string tmpLoadFile = @"C:\Users\francois\Documents\Visual Studio 2010\Projects\GenericTextFunctions\GenericTextFunctions\ABI\Catalogue 2012.rtf";
 			if (File.Exists(tmpLoadFile))
 			{
@@ -55,6 +59,40 @@ namespace GenericTextFunctions
 
 			LoadDragdropItems();
 			//DONE: WTF sometimes the dock does not display (on home pc) until minimized/maximized or clicked on the client area. Was because the expander was IsExpanded=false and then items are loaded into it via LoadDragdropItems();
+		}
+
+		protected override void OnSourceInitialized(EventArgs e)
+		{
+			base.OnSourceInitialized(e);
+			HwndSource source = PresentationSource.FromVisual(this) as HwndSource;
+			source.AddHook(WndProc);
+		}
+
+		private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+		{
+			WindowMessagesInterop.MessageTypes mt;
+			WindowMessagesInterop.ClientHandleMessage(msg, wParam, lParam, out mt);
+			if (mt == WindowMessagesInterop.MessageTypes.Show)
+			{
+				this.Show();
+				if (this.WindowState == System.Windows.WindowState.Minimized)
+					this.WindowState = System.Windows.WindowState.Normal;
+				bool tmptopmost = this.Topmost;
+				this.Topmost = true;
+				this.Topmost = tmptopmost;
+				this.Activate();
+				this.UpdateLayout();
+			}
+			else if (mt == WindowMessagesInterop.MessageTypes.Close)
+			{
+				this.Close();
+			}
+			else if (mt == WindowMessagesInterop.MessageTypes.Hide)
+			{
+				this.Hide();
+			}
+
+			return IntPtr.Zero;
 		}
 
 		private void LoadDragdropItems()
